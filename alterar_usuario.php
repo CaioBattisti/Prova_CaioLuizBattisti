4150d5138e37381862697112115172d6a8df6e04
CaioBattisti <cluizbattisti@gmail.com>
2025-08-20 20:08:32 -0300

    save23

--- alterar_usuario.php
+++ alterar_usuario.php
@@ -58,17 +58,29 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
         <form action="processa_alteracao_usuario.php" method="POST">
             <input type="hidden" name="id_usuario" value="<?= htmlspecialchars($usuario['id_usuario']) ?>">
             
-            
+            <label for="nome">Nome:</label> 
+            <input type="text" id="nome" name="nome" value="<?= htmlspecialchars($usuario['nome']) ?>" required>
 
+            <label for="email">Email:</label> 
+            <input type="email" id="email" name="email" value="<?= htmlspecialchars($usuario['email']) ?>" required>
 
-
+            <label for="id_perfil">Perfil:</label> 
+            <select id="id_perfil" name="id_perfil">
+                <option value="1"<?= $usuario['id_perfil'] == 1 ? 'selected': '' ?>>Administrador</option>
+                <option value="2"<?= $usuario['id_perfil'] == 2 ? 'selected': '' ?>>Secretária</option>
+                <option value="3"<?= $usuario['id_perfil'] == 3 ? 'selected': '' ?>>Almoxarife</option>
+                <option value="4"<?= $usuario['id_perfil'] == 4 ? 'selected': '' ?>>Cliente</option>
+            </select>
+            <!-- Se o Usuario logado for ADM, exibir opção de Alterar Senha -->
+            <?php if ($_SESSION['perfil'] == 1): ?>
+                <label for="nova_senha">Nova Senha:</label> 
+                <input type="password" id="senha" nome="senha" placeholder="Digite a nova senha (opcional)">           
+                <input type="password" id="nova-senha" name="nova_senha">
+                <?php endif; ?>
+            <button type="submit">Alterar</button>
+            <button type="reset">Cancelar</button>
         </form>
-
-
-
-
-
-
-
+    <?php endif; ?>
+    <a href="principal.php">Voltar</a>
 </body>
 </html>
