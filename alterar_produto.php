9207225a45a5abc095b27de27c5ff9fb3c97afef
CaioBattisti <cluizbattisti@gmail.com>
2025-08-26 20:31:50 -0300

    save66
    Relatório Geral: Inicio de PHP, apenas copiei e colei do busca de usuário, farei algumas alterações

---
+++ alterar_produto.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+// Verifica se o usuario tem permissão de ADM ou Secretária
+if ($_SESSION['perfil'] != 1 && $_SESSION['perfil'] != 2) {
+    echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
+    exit();
+}
+
+// Obtendo o Nome do Perfil do Usuario Logado
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+// Definição das Permissões por Perfil
+$permissoes = [
+    1=>["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+        "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+        "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+        "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
+
+    2=>["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php", "alterar_fornecedor.php"]],
+
+    3=>["Cadastrar"=>["cadastro_fornecedor.php", "cadastro_produto.php"],
+        "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+        "Alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+
+    4=>["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php"]],
+];
+
+// Obtendo as Opções Disponíveis para o Perfil Logado
+$opcoes_menu = $permissoes[$id_perfil];
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
