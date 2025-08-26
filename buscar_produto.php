5dac65c768472b0fbfd7ea8c84173cda78c78355
CaioBattisti <cluizbattisti@gmail.com>
2025-08-26 19:48:36 -0300

    save61
    relatório Geral: fazendo algumas pequenas alterações e adicionando mais de código aos poucos...

--- buscar_produto.php
+++ buscar_produto.php
@@ -17,17 +17,17 @@ $stmtPerfil->execute();
 $perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
 $nome_perfil = $perfil['nome_perfil'];
 
-// Definição das Permissões por Perfil
+// Definição das Permissões
 $permissoes = [
     1=>["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
         "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
         "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
         "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
-    
+
     2=>["Cadastrar"=>["cadastro_cliente.php"],
         "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
         "Alterar"=>["alterar_cliente.php", "alterar_fornecedor.php"]],
-    
+
     3=>["Cadastrar"=>["cadastro_fornecedor.php", "cadastro_produto.php"],
         "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
         "Alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
@@ -37,6 +37,21 @@ $permissoes = [
         "Buscar"=>["buscar_produto.php"],
         "Alterar"=>["alterar_cliente.php"]],
 ];
+$opcoes_menu = $permissoes[$id_perfil];
+
+// Inicializa variavel
+$produtos = [];
+
+
+
+
+
+
+
+
+
+
+
 
 
 
