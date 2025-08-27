9713610b53677613ac5ea331c9ac45fdeb441322
CaioBattisti <cluizbattisti@gmail.com>
2025-08-26 21:15:09 -0300

    save74
    Relatório Geral: fazendo as alterações, e adionando o código para buscar por id o produto e o metodo de delete sql(com proteção contra MySQLI).

--- excluir_produto.php
+++ excluir_produto.php
@@ -19,26 +19,47 @@ $nome_perfil = $perfil['nome_perfil'];
 
 // Definição das Permissões por Perfil
 $permissoes = [
-    1=>["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
-        "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
-        "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
-        "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
+    1=>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
+        "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
+        "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
+        "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
     2=>["Cadastrar"=>["cadastro_cliente.php"],
-        "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
-        "Alterar"=>["alterar_cliente.php", "alterar_fornecedor.php"]],
-    3=>["Cadastrar"=>["cadastro_fornecedor.php", "cadastro_produto.php"],
-        "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
-        "Alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
+        "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php","alterar_fornecedor.php"]],
+    3=>["Cadastrar"=>["cadastro_fornecedor.php","cadastro_produto.php"],
+        "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+        "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
         "Excluir"=>["excluir_produto.php"]],
     4=>["Cadastrar"=>["cadastro_cliente.php"],
         "Buscar"=>["buscar_produto.php"],
         "Alterar"=>["alterar_cliente.php"]],
 ];
 
+$opcoes_menu = $permissoes[$id_perfil];
 
+// Verifica permissão
+if ($_SESSION['perfil'] != 1 && $_SESSION['perfil'] != 3) {
+    echo "<script>alert('Acesso Negado!');window.location.href='principal.php';</script>";
+    exit();
+}
 
+// Lista todos os produtos
+$sql = "SELECT * FROM produto ORDER BY nome_prod ASC";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// Se receber id para excluir
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM produto WHERE id_produto = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
-
-
+    if ($stmt->execute()) {
+        echo "<script>alert('Produto excluído com sucesso!');window.location.href='excluir_produto.php';</script>";
+    } else {
+        echo "<script>alert('Erro ao excluir produto.');window.location.href='excluir_produto.php';</script>";
+    }
+}
 ?>
